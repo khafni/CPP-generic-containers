2b0dd93e45f776b2048df3d02f9c9d4ceb32aff2
pizza <hafnix@gmail.com>
2022-06-23 00:17:06 +0100

    br

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//main function of the program
+
+namespace ft {
+    int v = 69;
+}
+
+namespace ft {
+    int e = 420;
+}
+
+int v = 3;
+
+int main()
+{
+    std::cout << "ft::v = " << ft::v << std::endl;
+    std::cout << "ft::e = " << ft::e << std::endl;
+}
---
+++ vector.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace ft
+{
+    template<
+    class T, class Allocator = std::allocator<T>
+    > class vector
+    {
+        
+    }
+}
